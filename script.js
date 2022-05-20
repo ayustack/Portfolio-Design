cf196c60bad0060874e72d19dc800da81fbfb164
Ayush Jaiswal <jaiswalayush891@gmail.com>
2022-05-20 13:16:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+/* typing animation */
+var typeed = new Typed(".typing",{
+    strings:["","Software Developer Enthusiast.","Backend Engineer Enthusiast.","Competitive Coding Enthusiast."],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
